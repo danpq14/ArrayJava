d267863655597d79d873251adff12f235006d4b6
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-04-10 09:23:20 +0700

    Bài tập xóa phần tử khỏi mảng

---
+++ src/DeleteElementInArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class DeleteElementInArray {
+    public static void main(String[] args) {
+        int[] myArray = {1,2,3,5,1,6,7,5,7,8,9,5,4};
+        int x = 5;
+        delNumber(myArray,x);
+    }
+    static void delNumber (int[] arr, int number) {
+        for (int i=0; i< arr.length; i++) {
+            int index = 0;
+            int temp = 0;
+            if (arr[i] == number) {
+                index = i;
+                for (int j = index; j< arr.length - 1 ; j++ ) {
+                    temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i<arr.length; i++) {
+            if (arr[i] == number) arr[i] = 0;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
