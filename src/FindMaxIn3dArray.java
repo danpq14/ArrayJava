2e52401cd772d29b3230505e61d863c1603c3010
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-04-10 14:04:09 +0700

    Max in 2d Array

---
+++ src/FindMaxIn3dArray.java
@@ -0,0 +1,22 @@
+public class FindMaxIn3dArray {
+    public static void main(String[] args) {
+        int[][] myArray = {
+                {1,2,3,4,312},
+                {12,123,12,14,124},
+                {4,5,12,412,5,6,23,4142,523,1321},
+                {34,47,654,12,18273,1223}
+        };
+        findMaxin2dArray(myArray);
+    }
+    static void findMaxin2dArray(int[][] arr) {
+        int max = arr[0][0];
+        for (int i = 0; i<arr.length;i++) {
+            for (int j=0; j<arr[i].length; j++) {
+                if (max < arr[i][j]) {
+                    max = arr [i][j];
+                }
+            }
+        }
+        System.out.println("Max in Array is : "+ max);
+    }
+}
